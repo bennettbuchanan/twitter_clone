1d5942943790f4e4925808343f2d9c99d7580cab
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-24 19:14:48 -0800

    add post display javascript toggle behavior

---
+++ post_a_status.js
@@ -0,0 +1,12 @@
+function toggleFormVisibility()
+{
+  var frm_element = document.getElementById('subscribe_frm'); 
+
+  var vis = frm_element.style;
+  
+  if(vis.display=='' || vis.display=='none') {
+	  vis.display = 'block';
+  } else {
+	  vis.display = 'none';
+  }
+}
