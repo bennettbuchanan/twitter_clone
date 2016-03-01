ed7173d53ba67a1b317928c827d85f036c639c9b
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-01 14:34:12 -0800

    add data attributes and complete reply form functionality

--- reply.js
+++ reply.js
@@ -1,12 +1,25 @@
-var replyButton = document.querySelector('#user_id');
-var replyPost = document.querySelector('.replypost');
+var replyButton = document.querySelector('.statuscontentcontainer');
+var replyPost = document.querySelectorAll('.replypost');
 
-replyButton.addEventListener('click', doSomething, false);
+// Add click event listener to parent div
+replyButton.addEventListener("click",function(e) {
+	// e.target was the clicked element
+  	if (e.target.matches("a.replyLink")) {
+  	// store this particular link in a variable
+  	var thisReply = e.target.dataset.tag;
 
-function doSomething(e) {
-    if (replyPost.style.display == '' || replyPost.style.display == 'none') {
-        replyPost.style.display = 'block';
-    } else {
-        replyPost.style.display = 'none';
-    }
-}
+  		// call changeReply function
+		changeReply();
+
+		function changeReply() {
+			// Store the corresponding indexed replyPost in a variable
+		    var replyForm = replyPost[thisReply].style;
+		    // Change the display property based on the predefined style
+		   	if (replyForm.display == '' || replyForm.display == 'none') {
+		   	      replyForm.display = 'block';
+	   	    } else {
+		   	      replyForm.display = 'none';
+	        }
+		}
+	}
+});
