1fb61829cc24cc69ec2549442ac3a1b41a5f700b
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-02 18:33:35 -0800

    add hide header on scroll behavior

--- toggle.js
+++ toggle.js
@@ -6,3 +6,31 @@ function toggle(id) {
         elem.display = "none";
     }
 }
+
+window.addEventListener("scroll", function scrollDirection() {
+	var header = document.getElementsByTagName('header')[0].style;
+
+    var diffY=scrollDirection.y-window.pageYOffset;
+
+    // do not change header on mobile view
+    if (window.innerWidth > 768) {
+	    if (diffY < 0) {
+	        header.height = "0px";
+	    } else if (diffY > 0) {
+	        header.height = "43px";
+	    }
+   	}
+    scrollDirection.y=window.pageYOffset;
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('nav-button').addEventListener('click', function(){
+    var body = document.getElementsByTagName('body')[0];
+    if (body.className == 'nav-show') {
+      body.className = '';
+    }
+    else {
+      body.className = 'nav-show';
+    }
+  })
+});
