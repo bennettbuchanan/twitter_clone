1ebc918b40b16f4678416ca11080233820a7be2e
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-04 23:08:25 -0800

    add geolocation functionality

--- post_a_status.js
+++ post_a_status.js
@@ -2,5 +2,34 @@ var post_status = document.getElementById("post_button");
 
 post_status.addEventListener("click", function() {
 	var post_form = document.getElementById('post_form');
-	post_form.toggle();	
+	post_form.toggle();
 })
+
+// assign anonymous function to onsubmit property of form
+document.getElementById('post_status').onsubmit = function() {
+
+    // determine if the location checkbox is checked
+    var location = this.elements['location'];
+
+    if (location.checked) {
+
+    	getLocation();
+
+		function getLocation() {
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(showPosition);
+		    } else { 
+		        alert("Your status was posted!");
+		    }
+		}
+
+		function showPosition(position) {
+		    var y = "Latitude: " + position.coords.latitude + 
+		    "Longitude: " + position.coords.longitude;	
+		    alert("Your status was posted from " + y + "!");
+		}
+	    
+    } else {
+    	alert ("Your status was posted!");
+    }
+};
