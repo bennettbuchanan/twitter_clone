9d31de38fa3e50e0a769b9c2e6b99d1b472a2557
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-22 20:08:26 -0700

    move AJAX call JS to load_more.js file, adjust dates in json

--- load_more.js
+++ load_more.js
@@ -1,3 +1,48 @@
+// Get the template script.
+var theTemplateScript = document.getElementById("user-template");
+var content = theTemplateScript.innerHTML;
+var theTemplate = Handlebars.compile(content);
+
+function fetchJSON(xhttp) {
+  contextString = xhttp.responseText;
+  var contextObject = JSON.parse(contextString);
+
+  // Do not display "See more statuses" button if last page of data.
+  if(contextObject.last_page == true) {
+    var moreButton = document.getElementById('extra_statuses_button');
+    moreButton.style.display = "none";
+  }
+
+  // Loop through each item in the JSON array and create new div for a post.
+  for (var i = 0; i < contextObject.statuses.length; i++) {
+    var theCompiledHtml = theTemplate(contextObject.statuses[i]);
+    var div = document.createElement("div");
+    var element = document.getElementById("content-placeholder");
+    element.appendChild(div).innerHTML = theCompiledHtml;
+  }
+}
+
+function locationGet(url, fetchJSON) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        fetchJSON(xhttp);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+var load_statuses = document.getElementById("extra_statuses_button");
+
+// Set variable to count how many pages to load;
+var i = 1;
+load_statuses.addEventListener("click", function() {
+  locationGet('/statuses-' + i + '.json', fetchJSON);
+  i++;
+});
+
 // var load_statuses = document.getElementById("extra_statuses_button");
 //
 // load_statuses.addEventListener("click", function() {
--- weather.js
+++ weather.js
@@ -4,71 +4,7 @@ function getData(data) {
   document.getElementById('weather').appendChild(node);
 }
 
+// Append script to DOM for fetching data with callback.
 var script = document.createElement('script');
 script.src = 'https://holberton-weather-api.herokuapp.com/weather.js?jsonp_callback=getData'
 document.body.appendChild(script);
-
-
-
-
-
-  // Grab the template script
-  var theTemplateScript = document.getElementById("user-template");
-  var content = theTemplateScript.innerHTML;
-  var theTemplate = Handlebars.compile(content);
-
-  function fetchJSON(xhttp) {
-    contextString = xhttp.responseText;
-    var contextObject = JSON.parse(contextString);
-
-    console.log(contextObject.statuses[0])
-    console.log(contextObject.statuses);
-    console.log(contextObject.last_page);
-
-    if(contextObject.last_page == true) {
-      var moreButton = document.getElementById('extra_statuses_button');
-      moreButton.style.display = "none";
-    }
-
-    // Loop through each item in the JSON array and create new div for a post.
-    for (var i = 0; i < contextObject.statuses.length; i++) {
-      var theCompiledHtml = theTemplate(contextObject.statuses[i]);
-      var div = document.createElement("div");
-      var element = document.getElementById("content-placeholder");
-      element.appendChild(div).innerHTML = theCompiledHtml;
-    }
-  }
-
-  function locationGet(url, fetchJSON) {
-      var xhttp;
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          fetchJSON(xhttp);
-        }
-      };
-      xhttp.open("GET", url, true);
-      xhttp.send();
-  }
-
-  // Set variable to count how many pages to load;
-  var i = 1;
-
-  var load_statuses = document.getElementById("extra_statuses_button");
-
-  load_statuses.addEventListener("click", function() {
-    this.style.background = '#9C9898';
-    this.disabled = true;
-    this.style.cursor = 'not-allowed';
-    locationGet('/statuses-' + i + '.json', fetchJSON);
-    revertColor();
-    i++;
-  });
-
-  function revertColor () {
-    setTimeout(function() {
-      load_statuses.style.background = '#FF7679';
-      load_statuses.disabled = false;
-      load_statuses.style.cursor = 'pointer';
-    }, 2000);
-  }
